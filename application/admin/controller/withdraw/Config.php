ffabf284152343c0bca8e721ded24771bea1c97b
Z User <z@container>
2026-03-07 03:01:27 +0000

    fix: 修复提现配置保存失败问题，更正数据库表前缀为advn_

--- application/admin/controller/withdraw/Config.php
+++ application/admin/controller/withdraw/Config.php
@@ -3,7 +3,6 @@
 namespace app\admin\controller\withdraw;
 
 use app\common\controller\Backend;
-use app\common\library\SystemConfigService;
 use think\Db;
 
 /**
@@ -13,6 +12,24 @@ class Config extends Backend
 {
     protected $model = null;
     protected $noNeedRight = ['index'];
+
+    // 默认配置
+    protected $defaultConfig = [
+        'enabled' => '1',
+        'amounts' => '10,20,50,100',
+        'min_withdraw' => '1',
+        'max_withdraw' => '500',
+        'daily_withdraw_limit' => '3',
+        'daily_withdraw_amount' => '500',
+        'same_ip_limit' => '5',
+        'same_device_limit' => '3',
+        'auto_audit_amount' => '10',
+        'manual_audit_amount' => '50',
+        'new_user_withdraw_days' => '3',
+        'fee_rate' => '0',
+        'transfer_retry_count' => '3',
+        'transfer_retry_interval' => '300',
+    ];
 
     public function _initialize()
     {
@@ -34,11 +51,10 @@ class Config extends Backend
             Db::startTrans();
             try {
                 foreach ($data as $key => $value) {
-                    // 确保值是标量类型
                     if (!is_scalar($value)) {
                         continue;
                     }
-                    SystemConfigService::set('withdraw', $key, $value);
+                    $this->setConfig('withdraw_' . $key, $value);
                 }
                 
                 Db::commit();
@@ -49,53 +65,97 @@ class Config extends Backend
             }
         }
 
-        try {
-            // 获取默认配置
-            $defaults = SystemConfigService::getDefaults();
-            $defaultWithdraw = [];
-            if (is_array($defaults) && isset($defaults['withdraw']) && is_array($defaults['withdraw'])) {
-                $defaultWithdraw = $defaults['withdraw'];
-            }
-            
-            // 获取提现配置
-            $config = SystemConfigService::getWithdrawConfig();
-            
-            // 确保是数组
-            if (!is_array($config)) {
-                $config = [];
-            }
-            
-            // 合并默认值，确保所有字段都有值
-            $config = array_merge($defaultWithdraw, $config);
-            
-            // 确保所有值都是标量类型，避免模板渲染问题
-            foreach ($config as $key => $value) {
-                if (!is_scalar($value) && !is_null($value)) {
-                    $config[$key] = is_array($value) ? json_encode($value) : strval($value);
-                }
-            }
-            
-        } catch (\Exception $e) {
-            // 出错时使用默认配置
-            $config = [
-                'withdraw_enabled' => 1,
-                'withdraw_amounts' => '10,20,50,100',
-                'min_withdraw' => 1,
-                'max_withdraw' => 500,
-                'daily_withdraw_limit' => 3,
-                'daily_withdraw_amount' => 500,
-                'same_ip_limit' => 5,
-                'same_device_limit' => 3,
-                'auto_audit_amount' => 10,
-                'manual_audit_amount' => 50,
-                'new_user_withdraw_days' => 3,
-                'fee_rate' => 0,
-                'transfer_retry_count' => 3,
-                'transfer_retry_interval' => 300,
-            ];
-        }
+        // 读取配置
+        $config = $this->getConfig();
         
         $this->view->assign('config', $config);
         return $this->view->fetch();
+    }
+
+    /**
+     * 获取配置
+     */
+    protected function getConfig()
+    {
+        $config = $this->defaultConfig;
+        
+        try {
+            $list = Db::name('config')
+                ->where('name', 'like', 'withdraw_%')
+                ->select();
+            
+            foreach ($list as $item) {
+                $key = str_replace('withdraw_', '', $item['name']);
+                if (isset($config[$key])) {
+                    $config[$key] = $item['value'];
+                }
+            }
+        } catch (\Exception $e) {
+            // 使用默认配置
+        }
+        
+        return $config;
+    }
+
+    /**
+     * 设置配置
+     */
+    protected function setConfig($name, $value)
+    {
+        $exists = Db::name('config')
+            ->where('name', $name)
+            ->find();
+        
+        $time = time();
+        
+        if ($exists) {
+            Db::name('config')
+                ->where('name', $name)
+                ->update([
+                    'value' => $value,
+                    'updatetime' => $time,
+                ]);
+        } else {
+            Db::name('config')->insert([
+                'name' => $name,
+                'group' => 'withdraw',
+                'title' => $this->getConfigTitle($name),
+                'tip' => '',
+                'type' => 'string',
+                'value' => $value,
+                'content' => '',
+                'rule' => '',
+                'extend' => '',
+                'setting' => '',
+                'status' => 1,
+                'createtime' => $time,
+                'updatetime' => $time,
+            ]);
+        }
+    }
+
+    /**
+     * 获取配置标题
+     */
+    protected function getConfigTitle($name)
+    {
+        $titles = [
+            'withdraw_enabled' => '开启提现',
+            'withdraw_amounts' => '可选提现金额',
+            'withdraw_min_withdraw' => '最低提现金额',
+            'withdraw_max_withdraw' => '最高提现金额',
+            'withdraw_daily_withdraw_limit' => '每日提现次数',
+            'withdraw_daily_withdraw_amount' => '每日提现金额',
+            'withdraw_same_ip_limit' => '同IP提现次数',
+            'withdraw_same_device_limit' => '同设备提现次数',
+            'withdraw_auto_audit_amount' => '自动审核金额',
+            'withdraw_manual_audit_amount' => '人工审核金额',
+            'withdraw_new_user_withdraw_days' => '新用户提现天数',
+            'withdraw_fee_rate' => '提现手续费率',
+            'withdraw_transfer_retry_count' => '提现重试次数',
+            'withdraw_transfer_retry_interval' => '重试间隔',
+        ];
+        
+        return $titles[$name] ?? $name;
     }
 }
