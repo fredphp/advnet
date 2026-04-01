5f3665af3fc7d471c4d6b15cf863532bdca50aee
liaoh <fredphp52@gmail.com>
2026-04-01 17:20:45 +0800

    1

--- sql/install_singlepage.sql
+++ sql/install_singlepage.sql
@@ -1,10 +1,10 @@
 -- ==========================================
 -- 单页管理模块 SQL 迁移脚本
--- 表前缀: fa_
+-- 表前缀: advn_
 -- ==========================================
 
 -- 1. 创建单页分类表
-CREATE TABLE IF NOT EXISTS `fa_singlepage_category` (
+CREATE TABLE IF NOT EXISTS `advn_singlepage_category` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `name` varchar(100) NOT NULL DEFAULT '' COMMENT '分类名称',
   `description` varchar(500) NOT NULL DEFAULT '' COMMENT '分类描述',
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS `fa_singlepage_category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='单页分类表';
 
 -- 2. 创建单页表
-CREATE TABLE IF NOT EXISTS `fa_singlepage` (
+CREATE TABLE IF NOT EXISTS `advn_singlepage` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `category_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '分类ID',
   `title` varchar(200) NOT NULL DEFAULT '' COMMENT '页面标题',
@@ -37,51 +37,51 @@ CREATE TABLE IF NOT EXISTS `fa_singlepage` (
   KEY `idx_weigh` (`weigh`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='单页管理表';
 
--- 3. 插入菜单规则 (fa_auth_rule)
+-- 3. 插入菜单规则 (advn_auth_rule)
 -- 请根据你的实际父级菜单ID调整 pid 的值
 -- 假设顶级后台菜单 ID 为 0，插入一级菜单"单页管理"
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (0, 'singlepage', '单页管理', 'fa fa-file-text', '', '管理网站单页内容', 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 50, 'normal');
 
 -- 获取刚插入的单页管理菜单ID (用于子菜单的pid)
 SET @sp_parent_id = LAST_INSERT_ID();
 
 -- 单页分类管理
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_parent_id, 'singlepage/category', '分类管理', 'fa fa-folder-open', '', '管理单页分类', 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 10, 'normal');
 
 SET @sp_cat_id = LAST_INSERT_ID();
 
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_cat_id, 'singlepage/category/index', '查看', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_cat_id, 'singlepage/category/add', '添加', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_cat_id, 'singlepage/category/edit', '编辑', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_cat_id, 'singlepage/category/del', '删除', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_cat_id, 'singlepage/category/multi', '批量更新', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
 
 -- 单页内容管理
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_parent_id, 'singlepage/page', '单页管理', 'fa fa-file', '', '管理单页内容', 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 20, 'normal');
 
 SET @sp_page_id = LAST_INSERT_ID();
 
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_page_id, 'singlepage/page/index', '查看', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_page_id, 'singlepage/page/add', '添加', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_page_id, 'singlepage/page/edit', '编辑', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_page_id, 'singlepage/page/del', '删除', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
-INSERT INTO `fa_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
+INSERT INTO `advn_auth_rule` (`pid`, `name`, `title`, `icon`, `condition`, `remark`, `ismenu`, `createtime`, `updatetime`, `weigh`, `status`)
 VALUES (@sp_page_id, 'singlepage/page/multi', '批量更新', '', '', '', 0, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 'normal');
 
 -- 4. 插入一些示例分类数据
-INSERT INTO `fa_singlepage_category` (`name`, `description`, `weigh`, `status`, `createtime`, `updatetime`) VALUES
+INSERT INTO `advn_singlepage_category` (`name`, `description`, `weigh`, `status`, `createtime`, `updatetime`) VALUES
 ('关于我们', '公司介绍相关页面', 100, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
 ('帮助中心', '使用帮助相关页面', 90, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
 ('服务协议', '服务条款和隐私协议', 80, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
